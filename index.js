064be2d834f3b469e3044e14747a48bc28eaf5b1
reluxe3 <yoelfabricio123@gmail>
2025-05-07 16:12:06 -0500

    Agregamos package.json

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const http = require('http')
+
+function requestController(){
+    console.log('Bienvenidos al curso')
+}
+
+const server=http.createServer(requestController)
+const PORT=process.env.PORT
+
+server.listen(PORT, function() {
+    console.log("Aplicacion corriendo en : " + PORT)
+})
